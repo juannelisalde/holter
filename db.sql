ad3cff5461cae64999ce763941a061b9c47532ca
juan naranjo <juan.n.elisalde@gmail.com>
2017-08-21 19:26:37 -0500

    Se cambia sql

--- db.sql
+++ db.sql
@@ -84,3 +84,6 @@ INSERT INTO tipodocum (nombre, sigla, descripcion) VALUES
 
 INSERT INTO usuarios (nombres, apellidos, email, pass, tipo_usuario, creacion_usuario) VALUES 
 ('JUAN', 'NARANJO', 'juan.n.elisalde@gmail.com', SHA1('1234'), 'ADMIN', '2017-08-17 00:00:00');
+
+INSERT INTO parametros (frecardiacamin, frecardiacamax, cantidadmediciones, fecha_creacion) VALUES 
+('40', '180', '21', '2017-08-21 00:00:00');
